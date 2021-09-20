d3465c38c06aaa556cc32774f18425de7d9292e9
Chadwuo <41413559+Chadwuo@users.noreply.github.com>
2021-09-20 21:24:25 +0800

    当非管理员模式UAC启动报错； 添加demo

--- HHUpdateApp/Program.cs
+++ HHUpdateApp/Program.cs
@@ -48,11 +48,11 @@ namespace HHUpdateApp
                      * 当前用户是管理员的时候，直接启动应用程序 
                      * 如果不是管理员，则使用启动对象启动程序，以确保使用管理员身份运行 
                      */
-                     
+
                     //获得当前登录的Windows用户标示 
                     System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
                     System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
-                    
+
                     //判断当前登录用户是否为管理员 
                     if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
                     {
@@ -71,7 +71,7 @@ namespace HHUpdateApp
                                 //设置启动动作,确保以管理员身份运行 
                                 Verb = "runas",
 
-                                Arguments = " " + LaunchAppName
+                                Arguments = LaunchAppName + " " + CheckMode
                             };
                             //如果不是管理员，则启动UAC 
                             System.Diagnostics.Process.Start(startInfo);
